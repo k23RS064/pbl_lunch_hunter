fdc511ba5abb43f55e18155764f268f48886797f
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 12:38:45 +0900

    Update model.php

---
+++ src/model.php
@@ -0,0 +1,14 @@
+<?php
+class Model
+{
+    protected $table;
+    protected $db;
+    protected static $conf = [
+        'host'=>'mysql','user'=>'root','pass'=>'root','dbname'=>'test'
+    ];
+
+    protected static $codes = [
+        'user_type'=>['1'=>'社員','9'=>'管理者'],
+        
+    ]
+}
