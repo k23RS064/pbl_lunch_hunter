4fb3cc870e82b3a3acf16d858ff595af76d9733d
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-12-12 20:37:01 +0900

    Update pg_lookdata.php

--- src/pg_lookdata.php
+++ src/pg_lookdata.php
@@ -3,6 +3,7 @@ require_once('model.php');
 $user = new User();
 $rst = new Restaurant();
 $rev = new Review();
+$repo = new Report();
 
 echo '<pre>';
 var_dump($user->get_Userdetail(['user_id'=>'test']));
@@ -10,6 +11,13 @@ echo '<br>';
 var_dump($rst->get_RstDetail(['rst_id'=>'1']));
 echo '<br>';
 var_dump($rev->get_RevDettail(['review_id'=>'1']));
+echo '<br>';
+$reports = $repo->getList();
+var_dump($reports);
+echo '<br>';
+foreach ($reports as $report) {
+    var_dump($report);
+}
 echo '</pre>';
 
 //確認用URL：http://localhost/pbl_lunch_hunter/?do=pg_lookdata
